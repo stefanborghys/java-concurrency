28982e0105dacf76c026b6e8c5b62de5b6bee4a8
stefan borghys <stefan.borghys@seriousbusiness.be>
2014-06-15 19:06:29 +0200

    Added package comment
    Added package comment
    Maven javadoc plugin tryout

---
+++ java-concurrency/src/main/java/be/seriousbusiness/java/concurrency/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * Computer users take it for granted that their systems can do more than one thing at a time.</br>
+ * They assume that they can continue to work in a word processor, while other applications download files,</br>
+ * manage the print queue, and stream audio.</br>
+ * Even a single application is often expected to do more than one thing at a time.</br>
+ * For example, that streaming audio application must simultaneously read the digital audio off the network,</br>
+ * decompress it, manage playback, and update its display.</br>
+ * Even the word processor should always be ready to respond to keyboard and mouse events,</br>
+ * no matter how busy it is reformatting text or updating the display.</br>
+ * Software that can do such things is known as <i>concurrent</i> software.</br>
+ * </br>
+ * The Java platform is designed from the ground up to support concurrent programming,</br>
+ * with basic concurrency support in the Java programming language and the Java class libraries.</br>
+ * Since version 5.0, the Java platform has also included high-level concurrency APIs.</br>
+ * This lesson introduces the platform's basic concurrency support and summarizes some of the high-level APIs in the java.util.concurrent packages.
+ */
+package be.seriousbusiness.java.concurrency;
