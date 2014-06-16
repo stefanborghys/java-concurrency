db6a99e1f6dc1f9adee538e794037345981488c5
stefan borghys <stefan.borghys@seriousbusiness.be>
2014-06-16 18:24:27 +0200

    Corrected Thread Interference Example
    Corrected Thread Interference Example and it’s test

--- java-concurrency/src/main/java/be/seriousbusiness/java/concurrency/synchronization/ThreadInterferenceExample.java
+++ java-concurrency/src/main/java/be/seriousbusiness/java/concurrency/synchronization/ThreadInterferenceExample.java
@@ -3,6 +3,15 @@ package be.seriousbusiness.java.concurrency.synchronization;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Example implementation for Thread interference.</br>
+ * Interference happens when two operations, running in different threads, but acting on the same data, interleave.</br>
+ * This means that the two operations consist of multiple steps, and the sequences of steps overlap.</br<
+ * </br>
+ * source: <a href="http://docs.oracle.com/javase/tutorial/essential/concurrency/interfere.html"></a>
+ * @author seriousbusiness
+ *
+ */
 public class ThreadInterferenceExample extends Thread {
 	private static final Logger LOGGER=LoggerFactory.getLogger(ThreadInterferenceExample.class);
 	private Counter counter;
@@ -11,26 +20,35 @@ public class ThreadInterferenceExample extends Thread {
 		this.counter=counter;
 	}
 	
-	public void increment(){
-		counter.increment();
+	/**
+	 * Increments and decrements the counter 10 000 times.
+	 */
+	@Override
+	public void run(){
+		for(int i=0; i<100000; i++){
+			counter.increment();
+			counter.decrement();
+		}
 	}
 	
-	public void decrement(){
-		counter.decrement();
-	}
-	
-	public static void main(final String[] args){
+	/**
+	 * Creates 3 ThreadInterferenceExample objects and starts them.</br>
+	 * When all finished the number should still be 0.
+	 * @param args
+	 * @throws InterruptedException
+	 */
+	public static void main(final String[] args) throws InterruptedException{
 		final Counter counter=new Counter();
 		final ThreadInterferenceExample threadInterferenceExample1=new ThreadInterferenceExample(counter);
 		final ThreadInterferenceExample threadInterferenceExample2=new ThreadInterferenceExample(counter);
+		final ThreadInterferenceExample threadInterferenceExample3=new ThreadInterferenceExample(counter);
 		LOGGER.info("The counter number is: {}",counter.getNumber());
 		threadInterferenceExample1.start();
 		threadInterferenceExample2.start();
-		LOGGER.info("Increment threadInterferenceExample1");
-		threadInterferenceExample1.increment();
-		LOGGER.info("The counter number is: {}",counter.getNumber());
-		LOGGER.info("Decrement threadInterferenceExample2");
-		threadInterferenceExample2.decrement();
+		threadInterferenceExample3.start();
+		threadInterferenceExample1.join();
+		threadInterferenceExample2.join();
+		threadInterferenceExample3.join();
 		LOGGER.info("The counter number is: {}",counter.getNumber());
 	}
 
--- java-concurrency/src/test/java/be/seriousbusiness/java/concurrency/synchronization/ThreadInterferenceExampleTest.java
+++ java-concurrency/src/test/java/be/seriousbusiness/java/concurrency/synchronization/ThreadInterferenceExampleTest.java
@@ -1,6 +1,7 @@
 package be.seriousbusiness.java.concurrency.synchronization;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,25 +9,30 @@ import org.slf4j.LoggerFactory;
 public class ThreadInterferenceExampleTest {
 	private static final Logger LOGGER=LoggerFactory.getLogger(ThreadInterferenceExample.class);
 	
+	/**
+	 * This test creates 3 ThreadInterferenceExample objects and starts them.</br>
+	 * When all finished the number should still be 0</br>
+	 * as in total the counter is 30 000 times incremented and decremented, which results in 0</br>
+	 * but in reality it's not.
+	 * @throws InterruptedException
+	 */
+	@Ignore
 	@Test
-	public void testInterference(){
+	public void testInterference() throws InterruptedException{
 		final Counter counter=new Counter();
 		final ThreadInterferenceExample threadInterferenceExample1=new ThreadInterferenceExample(counter);
 		final ThreadInterferenceExample threadInterferenceExample2=new ThreadInterferenceExample(counter);
+		final ThreadInterferenceExample threadInterferenceExample3=new ThreadInterferenceExample(counter);
 		LOGGER.info("The counter number is: {}",counter.getNumber());
 		Assert.assertEquals("The default number should be 0",0,counter.getNumber());
 		threadInterferenceExample1.start();
 		threadInterferenceExample2.start();
-		for(int i=0; i<10000; i++){
-			LOGGER.info("Increment threadInterferenceExample1");
-			threadInterferenceExample1.increment();
-			LOGGER.info("The counter number is: {}",counter.getNumber());
-			Assert.assertEquals("The number should be 1",1,counter.getNumber());
-			LOGGER.info("Decrement threadInterferenceExample2");
-			threadInterferenceExample2.decrement();
-			LOGGER.info("The counter number is: {}",counter.getNumber());
-			Assert.assertEquals("The number should be 0",0,counter.getNumber());
-		}
+		threadInterferenceExample3.start();
+		threadInterferenceExample1.join();
+		threadInterferenceExample2.join();
+		threadInterferenceExample3.join();
+		LOGGER.info("The counter number is: {}",counter.getNumber());
+		Assert.assertEquals("The default number should still be 0",0,counter.getNumber());
 	}
 
 }
